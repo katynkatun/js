db948a5ad499f2fda4d99740a6416d7269f736eb
Евгений Хоменко <evgenijhomenko@MacBook-Air-Evgenij.local>
2019-02-02 17:24:42 +0200

    test vue-table2 FW

---
+++ vuetable-2-tutorial/src/components/MyVuetable.vue
@@ -0,0 +1,32 @@
+<template>
+  <vuetable ref="vuetable"
+    api-url="https://vuetable.ratiw.net/api/users"
+    :fields="fields"
+  ></vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+
+export default {
+  components: {
+    Vuetable
+  },
+  data () {
+    return {
+      fields: [
+        {
+          name:'__sequence',
+          title:'#'
+        },
+        'name', 'email', 'birthdate',
+        {
+          name:'address.line1',
+          title:'Address first'
+        },
+         'address.line2', 'address.zipcode'
+      ]
+    }
+  }
+}
+</script>
